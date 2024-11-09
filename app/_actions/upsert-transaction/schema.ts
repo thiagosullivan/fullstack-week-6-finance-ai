650c73adaac3c1727931837d0f66ac08f81acea5
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-09 04:12:02 -0300

    feat: ensure transaction can be updated

--- app/_actions/upsert-transaction/schema.ts
+++ app/_actions/upsert-transaction/schema.ts
@@ -1,11 +1,11 @@
 import {
-  TransactionType,
   TransactionCategory,
   TransactionPaymentMethod,
+  TransactionType,
 } from "@prisma/client";
 import { z } from "zod";
 
-export const addTransactionSchema = z.object({
+export const upsertTransactionSchema = z.object({
   name: z.string().trim().min(1),
   amount: z.number().positive(),
   type: z.nativeEnum(TransactionType),
